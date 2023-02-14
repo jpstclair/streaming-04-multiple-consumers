a6ac34901be3897b8a3c0325821972f05cc656a1
JamesStClair <james.stclair@omahaeval.org>
2023-02-13 20:47:52 -0600

    more edits

--- v3_emitter_of_tasks.py
+++ v3_emitter_of_tasks.py
@@ -1,6 +1,9 @@
 import pika
 import sys
 import webbrowser
+import csv
+import socket
+import time
 
 def offer_rabbitmq_admin_site():
     """Offer to open the RabbitMQ Admin website"""
@@ -21,39 +24,46 @@ def send_message(host: str, queue_name: str, message: str):
         message (str): the message to be sent to the queue
     """
 
-    try:
-        # create a blocking connection to the RabbitMQ server
-        conn = pika.BlockingConnection(pika.ConnectionParameters(host))
-        # use the connection to create a communication channel
-        ch = conn.channel()
-        # use the channel to declare a durable queue
-        # a durable queue will survive a RabbitMQ server restart
-        # and help ensure messages are processed in order
-        # messages will not be deleted until the consumer acknowledges
-        ch.queue_declare(queue=queue_name, durable=True)
-        # use the channel to publish a message to the queue
-        # every message passes through an exchange
-        ch.basic_publish(exchange="", routing_key=queue_name, body=message)
-        # print a message to the console for the user
-        print(f" [x] Sent {message}")
-    except pika.exceptions.AMQPConnectionError as e:
-        print(f"Error: Connection to RabbitMQ server failed: {e}")
-        sys.exit(1)
-    finally:
-        # close the connection to the server
-        conn.close()
+    host = "localhost"
+    port = 9999
+    address_tuple = (host, port)
 
-# Standard Python idiom to indicate main program entry point
-# This allows us to import this module and use its functions
-# without executing the code below.
-# If this is the program being run, then execute the code below
+    socket_family = socket.AF_INET 
+
+    socket_type = socket.SOCK_DGRAM 
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+
+    use_file = open("tasks.csv", "r")
+
+    reader = csv.reader(use_file, delimiter=',')
+
+    for row in reader:
+        input_file.read
+
+        fstring_use = f"{row}"
+
+        rabbit = fstring_use.encode()
+
+        sock.sendto(rabbit, address_tuple)
+        
+        time.sleep(5)
+
+        try:
+            conn = pika.BlockingConnection(pika.ConnectionParameters(host))
+            ch = conn.channel()
+            ch.queue_declare(queue=queue_name, durable=True)
+            ch.basic_publish(exchange="", routing_key=queue_name, body=MESSAGE)
+            print(f" [x] Sent {MESSAGE}")
+        except pika.exceptions.AMQPConnectionError as e:
+            print(f"Error: Connection to RabbitMQ server failed: {e}")
+            sys.exit(1)
+        finally:
+            conn.close()
+
 if __name__ == "__main__":  
-    # ask the user if they'd like to open the RabbitMQ Admin site
-    offer_rabbitmq_admin_site()
-    # get the message from the command line
-    # if no arguments are provided, use the default message
-    # use the join method to convert the list of arguments into a string
-    # join by the space character inside the quotes
-    message = " ".join(sys.argv[1:]) or "Second task....."
-    # send the message to the queue
-    send_message("localhost","task_queue2",message)
+    show_offer = False
+    offer_rabbitmq_admin_site(show_offer)
+    message = " ".join(sys.argv[1:])  or "{message}"
+    send_message("localhost","task_queue3",message)
+
